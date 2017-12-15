b235a519e4a9cf50db0061954e22465b367b2631
mrwgx3 <23343823+mrwgx3@users.noreply.github.com>
2017-12-14 17:46:54 -0700

    Rework RtcTemperature, GetTemperature()

--- src/RtcTemperature.h
+++ src/RtcTemperature.h
@@ -7,32 +7,53 @@
 class RtcTemperature
 {
 public:
-    RtcTemperature(int8_t degrees, uint8_t fraction) :
-        integerDegrees(degrees),
-        decimalFraction(fraction)
+    RtcTemperature( int8_t msdegC, uint8_t lsdegC ) :
+        mstemp( msdegC ),
+        lstemp( lsdegC & 0xC0 )  // Bind to RTC resolution
     {
     }
 
+//  Scaled integer temperature (x 256)
+    int16_t AsScaledDegrees()
+    {
+        return(  (mstemp << 8) + lstemp  );
+    }
+
+    // Rounded integer temperature (x 1)
+    int8_t AsRoundedDegrees()
+    {
+        // Equivalent to:
+        //   SF ( sign(x)    * floor(  abs(       x ) + 0.5    ) ) / SF, with
+        // =    ( sign(SF x) * floor(  abs(    SF x ) + 0.5 SF ) ) / SF  SF = 256
+        // =      sign(x)    * floor( sign(x)( SF x ) + 0.5 SF )   / SF
+        //
+        int8_t  sgnT = ( (mstemp < 0) ? -1 : 1 );
+        return( sgnT * ((sgnT * AsScaledDegrees() + 0x80) >> 8) );
+    }
+
+    // Float temperature (x 1)
     float AsFloat()
     {
-        float degrees = (float)integerDegrees;
-        degrees += (float)decimalFraction / ((degrees < 0) ? -100.0f : 100.0f) ;
-        return degrees;
+        return ( (float)AsScaledDegrees() / 256.0f );
     }
 
+    // Deprecated, included for backwards compatibility
     int8_t AsWholeDegrees()
     {
-        return integerDegrees;
+        int8_t  sgnT = ( (mstemp < 0) ? -1 : 1 );
+        return( sgnT * ((sgnT * AsScaledDegrees()) >> 8) ); // (x 1)
     }
 
+    // Deprecated, included for backwards compatibility
     uint8_t GetFractional()
     {
-        return decimalFraction;
+        int8_t  sgnT = ( (mstemp < 0) ? -1 : 1 );
+        return(   25 * ((uint8_t)(sgnT * lstemp) >> 6)  );  // (x 100)
     }
 
 protected:
-    int8_t integerDegrees;
-    uint8_t decimalFraction;
+    int8_t   mstemp;
+    uint8_t  lstemp;
 };
 
 #endif // __RTCTEMPERATURE_H__
