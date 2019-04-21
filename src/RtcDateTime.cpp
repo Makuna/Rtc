f287c1ee11f436c26b6a72d856877ecdddb0003b
Michael Miller <Makuna@hotmail.com>
2019-04-20 18:24:52 -0700

    Ds1302 date time valid (#92)
    * Validy Check for DateTime
    
    * LeapYearValidityTest

--- src/RtcDateTime.cpp
+++ src/RtcDateTime.cpp
@@ -9,6 +9,55 @@ RtcDateTime::RtcDateTime(uint32_t secondsFrom2000)
     _initWithSecondsFrom2000<uint32_t>(secondsFrom2000);
 }
 
+bool RtcDateTime::IsValid() const
+{
+    // this just tests the most basic validity of the value ranges
+    // and valid leap years
+    // It does not check any time zone or daylight savings time
+    if ((_month > 0 && _month < 13) &&
+        (_dayOfMonth > 0 && _dayOfMonth < 32) &&
+        (_hour < 24) &&
+        (_minute < 60) &&
+        (_second < 60))
+    {
+        // days in a month tests
+        //
+        if (_month == 2)
+        {
+            if (_dayOfMonth > 29)
+            {
+                return false;
+            }
+            else if (_dayOfMonth > 28)
+            {
+                // leap day
+                // check year to make sure its a leap year
+                uint16_t year = Year();
+
+                if ((year % 4) != 0)
+                {
+                    return false;
+                }
+
+                if ((year % 100) == 0 &&
+                    (year % 400) != 0)
+                {
+                    return false;
+                }
+            }
+        }
+        else if (_dayOfMonth == 31)
+        {
+            if ((((_month - 1) % 7) % 2) == 1)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    return false;
+}
 
 uint8_t StringToUint8(const char* pString)
 {
