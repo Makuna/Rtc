7a64ece395b0a5b291f6a4ff688cb4679f055086
Michael Miller <Makuna@hotmail.com>
2019-11-07 15:41:47 -0800

    Cross Platform ISR attribution (#102)

--- src/RtcUtility.h
+++ src/RtcUtility.h
@@ -7,6 +7,22 @@
 #include <inttypes.h>
 #endif
 
+// Arduino has no standard for attributing methods used for ISRs
+// even though some platforms require it, so to simplify the problem
+// for end users, this provides a standard ISR_ATTR
+#if !defined(ISR_ATTR)
+
+#if defined(ARDUINO_ARCH_ESP8266) 
+#define ISR_ATTR ICACHE_RAM_ATTR
+#elif defined(ARDUINO_ARCH_ESP32)
+#define ISR_ATTR ICACHE_RAM_ATTR
+#else
+#define ISR_ATTR
+#endif
+
+#endif // !defined(ISR_ATTR)
+
+
 // for some reason, the DUE board support does not define this, even though other non AVR archs do
 #ifndef _BV
 #define _BV(b) (1UL << (b))
